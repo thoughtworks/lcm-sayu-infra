a111396ba285ec6593be9eef07f7a0227820bc7a
Claudia Lopez <clopez@thoughtworks.com>
2020-08-31 15:52:19 -0400

    Creating bucket and Adding backend s3 for store the terraform state

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-west-2"
+  region = var.aws_region
   version = "~> 3.4.0"
+}
+terraform {
+  backend "s3" {
+    bucket = "***REMOVED***"
+    key    = "***REMOVED***"
+    region = "us-west-2"
+  }
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,4 @@
 aws_region = "us-west-2"
 aws_vpc_cidr_block = "10.0.0.0/16"
+app_name = "lcm-sayu"
+app_environment = "development"

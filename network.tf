323cd6ee30543e7078d1a35a810960582afda631
Claudia Lopez <clopez@thoughtworks.com>
2020-09-01 11:45:12 -0400

    Creating Elastic IP and NAT Gateway resources and adding tests

--- network.tf
+++ network.tf
@@ -52,3 +52,14 @@ resource "aws_route" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
+
+# Attach NAT gateway to private subnet
+resource "aws_eip" "nat" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.private.id
+  depends_on    = [aws_internet_gateway.main]
+}

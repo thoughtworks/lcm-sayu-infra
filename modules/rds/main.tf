d80b8f8895db606ee5a2d6aa2f41f896c73dc540
Kath Pino <Kath.pino@thoughtworks.com>
2020-09-22 18:09:12 -0300

    [dsantiba] #109 Add Security Group inbound rules RDS

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -44,6 +44,7 @@ resource "aws_security_group" "rds_sg" {
       from_port = 5432
       to_port   = 5432
       protocol  = "tcp"
+      security_group_id= aws_security_group.ecs_tasks.id
   }
 
   // outbound internet access
